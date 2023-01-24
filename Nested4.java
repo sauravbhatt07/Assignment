9a8be27b8d4e2fc00ee776b1f9ed934a31cc281d
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-01-24 17:19:30 +0530

    Add files via upload

---
+++ Nested4.java
@@ -0,0 +1,19 @@
+class Nested4{
+public static void main (String[]args){
+
+int num1=2;
+int num2=3;
+
+if (num1==3){
+if (num2==5){
+System.out.println("wrong statement");
+}
+
+else {
+System.out.println("Not matched");
+}}
+
+else{
+System.out.println("Not Satisfied");
+}
+}}
