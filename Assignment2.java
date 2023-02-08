0a76f7d3040db34f0c43fe65d0a2015028c9d294
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-02-08 15:33:12 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,13 @@
+package Feb8;
+
+public class Assignment2 {
+	 public static void main(String[] args){
+		 //Print Face structure like keywords in sop
+	        System.out.println(" +\"\"\"\"\"+ ");
+	        System.out.println("[| o o |]");
+	        System.out.println(" |  ^  | ");
+	        System.out.println(" | '-' | ");
+	        System.out.println(" +-----+ ");
+	    }
+		}
+
