0fbadcddb3bce8393a226d7aad655826a8e19bd9
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-21 21:43:10 +0530

    Add files via upload

--- Assignment.java
+++ Assignment.java
@@ -1,49 +1,21 @@
-package March20Exception;
+package Marcch21Exception;
+import java.util.Scanner;
 public class Assignment {
-	void display() {
-		try {
-		int i = 1/0;
-		System.out.println(i);
-		}
-		catch (Exception e) {
-		System.out.println("Invalid Devision");	
-		}}
-	void show() {
-		try {
-			String a= "India Is Best";
-			int n= Integer.parseInt(a);
+	 void addtwopositive() {
+			Scanner sc=new Scanner(System.in);
+			try {
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			System.out.println(" ");
+			String s="Non positive";
+			int n=Integer.parseInt(s);
 			System.out.println(n);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+		}}
+		public static void main(String[] args) {
+			Assignment c=new Assignment();
+				c.addtwopositive();
+			}
 		}
-			catch (Exception e) {
-			System.out.println("Format Mismatch");	
-			}}
-	void print() {
-		try {
-			int cal = 1+50/0;
-			System.out.println(cal);
-			}
-			catch (Exception e) {
-			System.out.println("Calculation Invalid");	
-			}}
-	void run() {
-		try {
-			int arr [] = new int[5];
-			arr[7]=10;
-			}
-			catch (Exception e) {
-			System.out.println("Array index is Invalid");	
-			}}
-	void s() {
-		System.out.println();
-		String s ="Exception handling completed";
-		System.out.println(s);	
-	}
-	public static void main(String[] args) {
-		Assignment ea =new Assignment();
-		ea.display();
-		ea.show();
-		ea.print();
-		ea.run();
-		ea.s();
-	}
-}
