a3a0125effdc3fe71bb4362c880a42ff53b2f4cc
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-28 17:54:27 +0530

    Add files via upload

--- Assignment.java
+++ Assignment.java
@@ -1,46 +1,9 @@
-package March22Exception;
-public class Assignment {
-	public static void main(String[] args) {
-		try {
-			try {
-				// Arithmetic exception
-				int i =50/0;
-				System.out.println(i);
-				}
-				catch(Exception e) {
-				System.out.println(e);
-				}
-			finally {
-				System.out.println("1st finally block after arithmetic exception ");
-			}
-				// null pointer exception
-				try {
-					String s = null;
-					System.out.println(s.length());
-				}
-				catch(Exception e) {
-					System.out.println(e);
-				}
-				finally {
-					System.out.println("2nd finally block after null pointer exception");
-				}
-				// ArrayIndexoutofbound exception
-				try {
-					int arr [] = new int[5];
-					arr[7]=10;
-				}
-				catch(Exception e) {
-					System.out.println(e);
-				}
-				finally {
-					System.out.println("3rd finally block after array index out of bound exception");
-				}
-			}
-			catch(Exception e) {
-				System.out.println(e);		
-			}
-		finally {
-			System.out.println("Differernt unchecked exceptions done");
-		}
-		}
+package March28;
+	class Assignment extends Thread {
+	public void run() {
+		System.out.println("ACBACBACBACBACABCA");
 	}
+	public static void main(String[] args) {
+		Assignment t = new Assignment();
+		t.start(); //move the thread to the active state
+	}}
