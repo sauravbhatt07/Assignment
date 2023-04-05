8aaf918afe9a89961a3bb86225e2133c596199e9
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-04-05 18:03:11 +0530

    Add files via upload

--- Assignment2.java
+++ Assignment2.java
@@ -1,13 +1,34 @@
-package Feb8;
+package April05IO;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+	public class Assignment2{ //main class 
+		public static void main(String[] args) throws IOException { 
+			//taking scanner for user input
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter 1 to store data:-");
+			int i =in.nextInt();
+		// for storing data
+			if (i==1) {
+				FileOutputStream fos = new FileOutputStream("D:\\11.txt");
+				String s ="Program stroing & writing data. "; 
+				byte b[] = s.getBytes(); 
+				fos.write(b); 
+				fos.close();
+				System.out.println("Storing data complete.");	
+				}
+			else { System.out.println("wrong keyword.");
+			}
+			System.out.println("Enter 2 to write data:-");
+			int j=in.nextInt();
+		// for writing data	
+			if (j==2) {
+					// file source
+					FileInputStream fis = new FileInputStream("D:\\11.txt");
+					int k=0;
+					while((i=fis.read())!=-1) { // loop for take all characters 
+						System.out.print((char)i); 
+					}}}}
 
-public class Assignment2 {
-	 public static void main(String[] args){
-		 //Print Face structure like keywords in sop
-	        System.out.println(" +\"\"\"\"\"+ ");
-	        System.out.println("[| o o |]");
-	        System.out.println(" |  ^  | ");
-	        System.out.println(" | '-' | ");
-	        System.out.println(" +-----+ ");
-	    }
-		}
-
+			
